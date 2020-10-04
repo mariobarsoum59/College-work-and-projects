2f237781eac22465200d8edf7ac7c723a3d119e8
mariobarsoum59 <71946511+mariobarsoum59@users.noreply.github.com>
2020-10-04 21:56:04 +0100

    Add files via upload

---
+++ Compute Coin Change/Coin Change/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Coin_Change
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterAmount = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.lblTwoEuro = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lblOneEuro = new System.Windows.Forms.Label();
+            this.txt0_50 = new System.Windows.Forms.TextBox();
+            this.lblFiftyCent = new System.Windows.Forms.Label();
+            this.txt0_20 = new System.Windows.Forms.TextBox();
+            this.lblTwentyCent = new System.Windows.Forms.Label();
+            this.txt0_10 = new System.Windows.Forms.TextBox();
+            this.lblTenCent = new System.Windows.Forms.Label();
+            this.txt0_05 = new System.Windows.Forms.TextBox();
+            this.lblFiveCent = new System.Windows.Forms.Label();
+            this.txt0_02 = new System.Windows.Forms.TextBox();
+            this.lblTwoCent = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnterAmount
+            // 
+            this.lblEnterAmount.AutoSize = true;
+            this.lblEnterAmount.Location = new System.Drawing.Point(28, 36);
+            this.lblEnterAmount.Name = "lblEnterAmount";
+            this.lblEnterAmount.Size = new System.Drawing.Size(74, 13);
+            this.lblEnterAmount.TabIndex = 0;
+            this.lblEnterAmount.Text = "Enter Amount:";
+            this.lblEnterAmount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(108, 33);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(48, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // lblTwoEuro
+            // 
+            this.lblTwoEuro.AutoSize = true;
+            this.lblTwoEuro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTwoEuro.Location = new System.Drawing.Point(28, 70);
+            this.lblTwoEuro.Name = "lblTwoEuro";
+            this.lblTwoEuro.Size = new System.Drawing.Size(34, 13);
+            this.lblTwoEuro.TabIndex = 2;
+            this.lblTwoEuro.Text = "£2.00";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(69, 70);
+            this.txt2.Name = "txt2";
+            this.txt2.ReadOnly = true;
+            this.txt2.Size = new System.Drawing.Size(33, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(69, 96);
+            this.txt1.Name = "txt1";
+            this.txt1.ReadOnly = true;
+            this.txt1.Size = new System.Drawing.Size(33, 20);
+            this.txt1.TabIndex = 5;
+            // 
+            // lblOneEuro
+            // 
+            this.lblOneEuro.AutoSize = true;
+            this.lblOneEuro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblOneEuro.Location = new System.Drawing.Point(28, 96);
+            this.lblOneEuro.Name = "lblOneEuro";
+            this.lblOneEuro.Size = new System.Drawing.Size(34, 13);
+            this.lblOneEuro.TabIndex = 4;
+            this.lblOneEuro.Text = "£1.00";
+            // 
+            // txt0_50
+            // 
+            this.txt0_50.Location = new System.Drawing.Point(69, 122);
+            this.txt0_50.Name = "txt0_50";
+            this.txt0_50.ReadOnly = true;
+            this.txt0_50.Size = new System.Drawing.Size(33, 20);
+            this.txt0_50.TabIndex = 7;
+            // 
+            // lblFiftyCent
+            // 
+            this.lblFiftyCent.AutoSize = true;
+            this.lblFiftyCent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblFiftyCent.Location = new System.Drawing.Point(28, 122);
+            this.lblFiftyCent.Name = "lblFiftyCent";
+            this.lblFiftyCent.Size = new System.Drawing.Size(34, 13);
+            this.lblFiftyCent.TabIndex = 6;
+            this.lblFiftyCent.Text = "£0.50";
+            // 
+            // txt0_20
+            // 
+            this.txt0_20.Location = new System.Drawing.Point(69, 148);
+            this.txt0_20.Name = "txt0_20";
+            this.txt0_20.ReadOnly = true;
+            this.txt0_20.Size = new System.Drawing.Size(33, 20);
+            this.txt0_20.TabIndex = 9;
+            // 
+            // lblTwentyCent
+            // 
+            this.lblTwentyCent.AutoSize = true;
+            this.lblTwentyCent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTwentyCent.Location = new System.Drawing.Point(28, 148);
+            this.lblTwentyCent.Name = "lblTwentyCent";
+            this.lblTwentyCent.Size = new System.Drawing.Size(34, 13);
+            this.lblTwentyCent.TabIndex = 8;
+            this.lblTwentyCent.Text = "£0.20";
+            // 
+            // txt0_10
+            // 
+            this.txt0_10.Location = new System.Drawing.Point(69, 174);
+            this.txt0_10.Name = "txt0_10";
+            this.txt0_10.ReadOnly = true;
+            this.txt0_10.Size = new System.Drawing.Size(33, 20);
+            this.txt0_10.TabIndex = 11;
+            // 
+            // lblTenCent
+            // 
+            this.lblTenCent.AutoSize = true;
+            this.lblTenCent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTenCent.Location = new System.Drawing.Point(28, 174);
+            this.lblTenCent.Name = "lblTenCent";
+            this.lblTenCent.Size = new System.Drawing.Size(34, 13);
+            this.lblTenCent.TabIndex = 10;
+            this.lblTenCent.Text = "£0.10";
+            // 
+            // txt0_05
+            // 
+            this.txt0_05.Location = new System.Drawing.Point(69, 200);
+            this.txt0_05.Name = "txt0_05";
+            this.txt0_05.ReadOnly = true;
+            this.txt0_05.Size = new System.Drawing.Size(33, 20);
+            this.txt0_05.TabIndex = 13;
+            // 
+            // lblFiveCent
+            // 
+            this.lblFiveCent.AutoSize = true;
+            this.lblFiveCent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblFiveCent.Location = new System.Drawing.Point(28, 200);
+            this.lblFiveCent.Name = "lblFiveCent";
+            this.lblFiveCent.Size = new System.Drawing.Size(34, 13);
+            this.lblFiveCent.TabIndex = 12;
+            this.lblFiveCent.Text = "£0.05";
+            // 
+            // txt0_02
+            // 
+            this.txt0_02.Location = new System.Drawing.Point(69, 226);
+            this.txt0_02.Name = "txt0_02";
+            this.txt0_02.ReadOnly = true;
+            this.txt0_02.Size = new System.Drawing.Size(33, 20);
+            this.txt0_02.TabIndex = 15;
+            // 
+            // lblTwoCent
+            // 
+            this.lblTwoCent.AutoSize = true;
+            this.lblTwoCent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTwoCent.Location = new System.Drawing.Point(28, 226);
+            this.lblTwoCent.Name = "lblTwoCent";
+            this.lblTwoCent.Size = new System.Drawing.Size(34, 13);
+            this.lblTwoCent.TabIndex = 14;
+            this.lblTwoCent.Text = "£0.02";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(118, 70);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 18;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(69, 252);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 307);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txt0_02);
+            this.Controls.Add(this.lblTwoCent);
+            this.Controls.Add(this.txt0_05);
+            this.Controls.Add(this.lblFiveCent);
+            this.Controls.Add(this.txt0_10);
+            this.Controls.Add(this.lblTenCent);
+            this.Controls.Add(this.txt0_20);
+            this.Controls.Add(this.lblTwentyCent);
+            this.Controls.Add(this.txt0_50);
+            this.Controls.Add(this.lblFiftyCent);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblOneEuro);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.lblTwoEuro);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblEnterAmount);
+            this.Name = "Form1";
+            this.Text = "Coin Change ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterAmount;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label lblTwoEuro;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lblOneEuro;
+        private System.Windows.Forms.TextBox txt0_50;
+        private System.Windows.Forms.Label lblFiftyCent;
+        private System.Windows.Forms.TextBox txt0_20;
+        private System.Windows.Forms.Label lblTwentyCent;
+        private System.Windows.Forms.TextBox txt0_10;
+        private System.Windows.Forms.Label lblTenCent;
+        private System.Windows.Forms.TextBox txt0_05;
+        private System.Windows.Forms.Label lblFiveCent;
+        private System.Windows.Forms.TextBox txt0_02;
+        private System.Windows.Forms.Label lblTwoCent;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Compute Coin Change/Coin Change/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coin_Change
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static double[] coins = { 0.02, 0.05, 0.10, 0.20, 0.50, 1.00, 2.00 };
+        static int n = coins.Length;
+
+
+
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(txtAmount.Text);
+
+            List<double> ans = new List<double>();//holds the coins in the list
+
+            // Traverse through coins
+            for (int i = n - 1; i >= 0; i--)
+            {
+                // Find coins 
+                while (amount >= coins[i])
+                {
+                    amount -= coins[i];
+                    ans.Add(coins[i]);
+                }
+            }
+
+            //counter for each coin
+            int TwoCount = 0;
+            int OneCount = 0;
+            int FiftyCount = 0;
+            int TwentyCount = 0;
+            int tenCount = 0;
+            int fiveCount = 0;
+            int twoCount = 0;
+
+
+            foreach (var i in ans)
+            {
+                if (i == 2.00)
+                {
+                    TwoCount++;
+                }
+                if (i == 1.00)
+                {
+                    OneCount++;
+                }
+                if (i == 0.50)
+                {
+                    FiftyCount++;
+                }
+                if (i == 0.20)
+                {
+                    TwentyCount++;
+                }
+                if (i == 0.10)
+                {
+                    tenCount++;
+                }
+                if (i == 0.05)
+                {
+                    fiveCount++;
+                }
+                if (i == 0.02)
+                {
+                    twoCount++;
+                }
+
+            }
+            //display apropiate count in each box
+            txt2.Text = TwoCount.ToString("g");
+            txt1.Text = OneCount.ToString("g");
+            txt0_50.Text = FiftyCount.ToString("g");
+            txt0_20.Text = TwentyCount.ToString("g");
+            txt0_10.Text = tenCount.ToString("g");
+            txt0_05.Text = fiveCount.ToString("g");
+            txt0_02.Text = twoCount.ToString("g");
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
